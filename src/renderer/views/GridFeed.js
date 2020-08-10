f796d83958c324f73b27bce2455161794d50f597
Christopher <chrisgraneyward@gmail.com>
2020-08-10 15:18:06 +0100

    feat: implement feed pagnation
    Allows pagnation (lazy loading) indefiniteley on the /new and /trending pages.

--- src/renderer/views/GridFeed.js
+++ src/renderer/views/GridFeed.js
@@ -4,10 +4,12 @@ import VideoWidget from "../components/video/VideoWidget";
 class GridFeed extends React.Component {
     constructor() {
         super();
-        this.state = { data: [] };
+        this.state = { data: [], lazyLoading: false };
+        this.handleScroll = this.handleScroll.bind(this);
     }
 
     componentDidMount() {
+        window.addEventListener("scroll", this.handleScroll);
         fetch(`https://3speak.online/apiv2/feeds/${this.props.type}`)
             .then(res => res.json())
             .then(json => {
@@ -15,13 +17,55 @@ class GridFeed extends React.Component {
             })
     }
 
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
+    }
+
+    handleScroll() {
+        if (!this.state.lazyLoading) {
+            const windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
+            const body = document.body;
+            const html = document.documentElement;
+            const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+            const windowBottom = windowHeight + window.pageYOffset;
+            if (windowBottom + 200 >= docHeight) {
+                this.setState({
+                    lazyLoading: true
+                });
+                fetch(`https://3speak.online/api/${this.props.type === 'new' ? 'new' : 'trends'}/more?skip=${this.state.data.length}`)
+                    .then(res => res.json())
+                    .then(json => {
+                        json = json.recommended ? json.recommended : json.trends
+                        json.forEach((video) => {
+                            video['author'] = video['owner'];
+                            delete video['owner'];
+                        });
+                        json = this.state.data.concat(json)
+                        json = json.filter((video, index, self) =>
+                            index === self.findIndex((v) => (
+                                v.author === video.author && v.permlink === video.permlink
+                            ))
+                        )
+                        console.log(json)
+                        this.setState({
+                            data: json,
+                            lazyLoading: false
+                        })
+                    })
+            }
+        }
+    }
+
     render() {
         return (
-                <div className='row'>
+            <div>
+                <h1>{this.props.type}</h1>
+                <div className={'row'}>
                 {this.state.data.map(el => (
                     <VideoWidget key={el.author + '/' + el.permlink} reflink={`hive:${el.author}:${el.permlink}`} {...el} />
                 ))}
                 </div>
+            </div>
         );
     }
 }
