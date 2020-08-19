ff7557db8285353850a68447a2a5df0ccd63b7f1
Christopher <chrisgraneyward@gmail.com>
2020-08-19 13:11:21 +0100

    feat: add reflink source switching to gridfeed
    Take source as an optional prop that defaults to hive for now. Add account provider switching for further down the road.

--- src/renderer/views/GridFeed.js
+++ src/renderer/views/GridFeed.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import VideoWidget from "../components/video/VideoWidget";
+import RefLink from "../../main/RefLink";
 
 class GridFeed extends React.Component {
     constructor(props) {
         super(props);
         // pass awaitingMoreData as true to prevent lazy loading
-        let {awaitingMoreData = undefined} = props
-        this.state = { data: [], awaitingMoreData}
+        let {awaitingMoreData = undefined, source = "hive"} = props
+        this.state = { data: [], awaitingMoreData, reflink: RefLink.parse(source)}
         this.handleScroll = this.handleScroll.bind(this);
     }
 
     componentDidMount() {
         window.addEventListener("scroll", this.handleScroll);
-        fetch(`https://3speak.online/apiv2/feeds/${this.props.type}`)
-            .then(res => res.json())
-            .then(json => {
-                this.setState({data: json})
-            })
+        switch(this.state.reflink.source.value) {
+            case "hive": {
+                fetch(`https://3speak.online/apiv2/feeds/${this.props.type}`)
+                    .then(res => res.json())
+                    .then(json => {
+                        this.setState({data: json})
+                    })
+            }
+            case "orbitdb": {
+
+            }
+            default: {
+
+            }
+        }
+
     }
 
     componentWillUnmount() {
@@ -34,26 +46,36 @@ class GridFeed extends React.Component {
                 this.setState({
                     awaitingMoreData: true
                 });
-                fetch(`https://3speak.online/api/${this.props.type === 'new' ? 'new' : 'trends'}/more?skip=${this.state.data.length}`)
-                    .then(res => res.json())
-                    .then(json => {
-                        json = json.recommended ? json.recommended : json.trends
-                        json.forEach((video) => {
-                            video['author'] = video['owner'];
-                            delete video['owner'];
-                        });
-                        json = this.state.data.concat(json)
-                        json = json.filter((video, index, self) =>
-                            index === self.findIndex((v) => (
-                                v.author === video.author && v.permlink === video.permlink
-                            ))
-                        )
-                        console.log(json)
-                        this.setState({
-                            data: json,
-                            awaitingMoreData: false
-                        })
-                    })
+                switch(this.state.reflink.source.value) {
+                    case "hive": {
+                        fetch(`https://3speak.online/api/${this.props.type === 'new' ? 'new' : 'trends'}/more?skip=${this.state.data.length}`)
+                            .then(res => res.json())
+                            .then(json => {
+                                json = json.recommended ? json.recommended : json.trends
+                                json.forEach((video) => {
+                                    video['author'] = video['owner'];
+                                    delete video['owner'];
+                                });
+                                json = this.state.data.concat(json)
+                                json = json.filter((video, index, self) =>
+                                    index === self.findIndex((v) => (
+                                        v.author === video.author && v.permlink === video.permlink
+                                    ))
+                                )
+                                this.setState({
+                                    data: json,
+                                    awaitingMoreData: false
+                                })
+                            })
+                    }
+                    case "orbitdb": {
+
+                    }
+                    default: {
+
+                    }
+                }
+
             }
         }
     }
