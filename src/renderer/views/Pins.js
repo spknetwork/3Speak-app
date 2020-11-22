91e205fa4eaecd0f03700202ed2a5f56fe6798dc
Christopher Graney-Ward <chrisgraneyward@gmail.com>
2020-11-22 23:50:06 +0000

    feat: add searching functionality
    Ability to search for, add and remove pins from the main trending and new page as well as for communities.

--- src/renderer/views/Pins.js
+++ src/renderer/views/Pins.js
@@ -8,6 +8,8 @@ import Popup from 'react-popup';
 import Utils from '../utils';
 import CID from 'cids'
 import RefLink from '../../main/RefLink'
+import Debug from "debug";
+const debug = Debug("blasio:pins")
 const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
     <a
         href=""
@@ -54,7 +56,9 @@ class Pins extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            pinls: []
+            pinls: [],
+            newVideos: [],
+            trendingVideos: []
         }
         this.pid = null;
         this.generate = this.generate.bind(this)
@@ -62,6 +66,38 @@ class Pins extends Component {
     async componentDidMount() {
         await this.generate();
         this.pid = setInterval(this.generate, 1500)
+        this.updateSearchTables()
+    }
+    updateSearchTables( community = null ) {
+        let ids = this.state.pinls.map(x => {return x._id})
+        console.log(ids)
+        let newUrl = 'https://3speak.co/apiv2/feeds/new?limit=10'
+        let trendingUrl = 'https://3speak.co/apiv2/feeds/trending?limit=10'
+        if (community) {
+            newUrl = `https://3speak.co/apiv2/feeds/community/${community}/new?limit=10`
+            trendingUrl = `https://3speak.co/apiv2/feeds/community/${community}/trending?limit=10`
+        }
+        fetch(newUrl)
+            .then(r => r.json())
+            .then(r => {
+                for (let video of r) {
+                    let id = `hive:${video.author}:${video.permlink}`
+                    video.isPinned = ids.includes(id);
+                    video.id = id;
+                }
+                console.log(r)
+                this.setState({newVideos: r})
+            })
+        fetch(trendingUrl)
+            .then(r => r.json())
+            .then(r => {
+                for (let video of r) {
+                    let id = `hive:${video.author}:${video.permlink}`;
+                    video.isPinned = ids.includes(id);
+                    video.id = id;
+                }
+                this.setState({trendingVideos: r})
+            })
     }
     componentWillUnmount() {
         clearInterval(this.pid)
@@ -71,6 +107,25 @@ class Pins extends Component {
         this.setState({
             pinls
         })
+    }
+    async PinLocally(cids, title, _id) {
+        debug(`CIDs to store ${JSON.stringify(cids)}`)
+        if(cids.length !== 0) {
+            NotificationManager.info("Pinning in progress")
+            await PromiseIpc.send("pins.add", {
+                _id,
+                source: "Pins page",
+                cids,
+                expire: null,
+                meta: {
+                    title
+                }
+            })
+            NotificationManager.success(`Video with reflink of ${this.state.reflink} has been successfully pinned! Thank you for contributing!`, "Pin Successful")
+        } else {
+            NotificationManager.warning("This video is not available on IPFS")
+        }
+        await this.generate()
     }
     async ManualAdd() {
 
@@ -232,6 +287,49 @@ class Pins extends Component {
                     {rows}
                 </tbody>
             </Table>
+            <h6>Select to pin and help secure the network by backing up videos</h6>
+            <input type='text' placeholder='Enter community ID...' onChange={(event) => {
+                if (event.target.value.match(/\bhive-\d{6}\b/g)) {
+                    this.updateSearchTables(event.target.value)
+                }
+            }} />
+            <Row>
+                {['new', 'trending'].map(type => (
+                    <Col>
+                        <Table striped bordered hover size='sm'>
+                            <thead>
+                            <tr>
+                                <th>{type} videos</th>
+                                <th>Title</th>
+                                <th>Creator</th>
+                                <th>pinned</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {this.state[`${type}Videos`].map(video => (
+                                <tr>
+                                    <td><img src={video.images.thumbnail} /></td>
+                                    <td>{video.title}</td>
+                                    <td>{video.author}</td>
+                                    <td>{video.isPinned ? (
+                                        <Button variant="danger" onClick={async() => {
+                                            await this.removePin(video.id)
+                                            this.updateSearchTables()
+                                        }}>X</Button>
+                                    ) : (
+                                        <button variant="success" onClick={async() => {
+                                            await this.PinLocally([video.ipfs], video.title, video.id)
+                                            this.updateSearchTables()
+                                        }}>O</button>
+                                    )}</td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </Table>
+                    </Col>
+                ))}
+
+            </Row>
         </div>);
     }
 }
