c18a8cea1d96991630e380f9363fc69102056578
vaultec <47548474+vaultec81@users.noreply.github.com>
2021-04-30 23:15:12 -0700

    feat: initial support for comment operations

--- src/renderer/components/video/CommentSection.js
+++ src/renderer/components/video/CommentSection.js
@@ -1,12 +1,67 @@
-import React, { Component } from 'react';
+import React, { Component, useCallback, useRef } from 'react';
 import PostComment from './PostComment';
 import PromiseIpc from 'electron-promise-ipc';
 import utils from '../../utils';
+import { Form } from 'react-bootstrap';
+import './CommentSection.css'
+import RefLink from '../../../main/RefLink'
+import randomstring from 'randomstring'
+
+function ConnectAccountNotice() {
+    return (
+        <p className="alert alert-info">To comment on this video please connect a HIVE account to your
+        profile:
+            &nbsp;<a href="#/login" class="keychainify-checked">Connect HIVE Account</a>
+        </p>
+    )
+}
+
+function NoComments() {
+    return (
+        <div class="alert alert-info">
+            This video has no comments yet. To write a comment login and click the "Reply" button below the video player.
+        </div>
+    )
+}
+
+function CommentForm(props) {
+    const commentBodyRef = useRef()
+    const { parent_reflink } = props
+
+    const postComment = useCallback(async() => {
+        console.log("posting")
+        console.log(commentBodyRef.current.value)
+        console.log(parent_reflink)
+        const [networkId, parent_author, parent_permlink] = RefLink.parse(parent_reflink).link
+        console.log(parent_author, parent_permlink)
+        await utils.acctOps.postComment({
+            accountType: "hive",
+            body: commentBodyRef.current.value,
+            parent_author,
+            parent_permlink,
+            username: 'sisy',
+            permlink: `re-${parent_permlink}-${randomstring.generate({
+                length: 8,
+                charset: 'alphabetic'
+            }).toLowerCase()}}`,
+            title: ''
+        })
+    }, [parent_reflink])
+    return (<React.Fragment>
+        <textarea id="new-comment-body" className="form-control w-100" ref={commentBodyRef} placeholder="Comment here..." maxLength="25000">
+
+        </textarea>
+        <button id="new-comment-btn" className="btn mt-1 btn-primary float-right" onClick={postComment}>
+            Comment
+        </button>
+    </React.Fragment>)
+}
+
 
 class CommentSection extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             commentGraph: null,
             totalComments: 0
         }
@@ -23,7 +78,7 @@ class CommentSection extends Component {
     async generateComments(reflink) {
         let totalComments = 0;
         let comments = [];
-        for await(const rootComment of this.generateCommentGraph(reflink, () => {
+        for await (const rootComment of this.generateCommentGraph(reflink, () => {
             totalComments++;
         })) {
             comments.push(rootComment);
@@ -39,42 +94,55 @@ class CommentSection extends Component {
      * @param {Function} progressHandler 
      */
     async *generateCommentGraph(startRoot, progressHandler) {
-        const children = await PromiseIpc.send("distiller.getChildren", startRoot, {asPost: true});
-        for(const child of children) {
-            if((await PromiseIpc.send("blocklist.has", child._id))) {
+        const children = await PromiseIpc.send("distiller.getChildren", startRoot, { asPost: true });
+        for (const child of children) {
+            /*if ((await PromiseIpc.send("blocklist.has", child._id))) {
                 //Comment blocking; Stop working on this tree.
                 //Add warning for lower level blocks later on.
                 continue;
-            }
+            }*/
             let childComments = [];
-            for await(const childComment of this.generateCommentGraph(child._id, progressHandler)) {
+            for await (const childComment of this.generateCommentGraph(child._id, progressHandler)) {
+                console.log(childComment)
                 childComments.push(childComment)
             }
-            var childVideoInfo = await utils.accounts.permalinkToVideoInfo(child._id)
-            const proc = <div key={child._id} className="box mb-3">
-                <PostComment reflink={child._id} commentInfo={childVideoInfo}></PostComment>
-                {childComments}
-            </div>;
-            yield proc;
-            if(progressHandler) {
-                progressHandler();
+            console.log("pass 111")
+
+            try {
+                var childVideoInfo = await utils.accounts.permalinkToVideoInfo(child._id)
+                console.log(childVideoInfo)
+                const proc = <div key={child._id} className="box mb-3">
+                    <PostComment reflink={child._id} commentInfo={childVideoInfo}></PostComment>
+                    {childComments}
+                </div>;
+                yield proc;
+                if (progressHandler) {
+                    progressHandler();
+                }
+            } catch (ex) {
+                console.log(ex)
+                continue;
             }
         }
-        return;
     }
-    render() { 
+    render() {
+        var isLogged = !!localStorage.getItem("SNProfileID")
         return (<React.Fragment>
             <h6 className="comment-count">Comments: ({this.state.totalComments})</h6>
-            <div className="box mb-3">
+            <div className="box mb-3 clearfix">
+                <h6 class="text-muted">Reply:</h6>
                 {
-                    //Future comment editor box
+                    isLogged ? <CommentForm parent_reflink={this.props.reflink}/> : <ConnectAccountNotice />
                 }
+
             </div>
             {
-                this.state.commentGraph
+                this.state.totalComments === 0 ? <NoComments /> : this.state.commentGraph
             }
         </React.Fragment>);
     }
+    static CommentForm() {
+        return CommentForm;
+    }
 }
- 
 export default CommentSection;
--- src/renderer/components/video/PostComment.js
+++ src/renderer/components/video/PostComment.js
@@ -1,17 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component, useRef, useCallback } from 'react';
 import Utils from '../../utils';
 import RefLink from '../../../main/RefLink'
 import ReactMarkdown from 'react-markdown';
 import { FaThumbsUp, FaThumbsDown, FaDollarSign, FaTimesCircle } from "react-icons/fa";
 import { BsThreeDotsVertical } from 'react-icons/bs'
-import { Dropdown } from 'react-bootstrap';
+import { Dropdown, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import Vote from "./Vote";
 import DOMPurify from 'dompurify';
 const electronIpc = require('electron-promise-ipc');
 const { clipboard } = require('electron');
 import ArraySearch from 'arraysearch';
 const Finder = ArraySearch.Finder;
-import {NotificationManager} from 'react-notifications'
+import { NotificationManager } from 'react-notifications';
+import CommentSection from './CommentSection';
+import randomstring from 'randomstring';
+import Convert from 'convert-units';
+import DateAndTime from 'date-and-time'
+
+
+function NormaliseTime(val, unit) {
+    return Math.round(Convert(val).from(unit).toBest().val) + Convert(val).from(unit).toBest().unit
+}
+
+function CommentForm(props) {
+    const commentBodyRef = useRef()
+    const { parent_reflink } = props
+    console.log(parent_reflink)
+    const postComment = useCallback(async () => {
+        console.log("posting")
+        console.log(commentBodyRef.current.value)
+        console.log(parent_reflink)
+        const [networkId, parent_author, parent_permlink] = RefLink.parse(parent_reflink).link
+        console.log(parent_author, parent_permlink)
+        await Utils.acctOps.postComment({
+            accountType: "hive",
+            body: commentBodyRef.current.value,
+            parent_author,
+            parent_permlink,
+            username: 'sisy',
+            permlink: `re-${parent_permlink}-${randomstring.generate({
+                length: 8,
+                charset: 'alphabetic'
+            }).toLowerCase()}`,
+            title: ''
+        })
+    }, [parent_reflink])
+    return (<React.Fragment>
+        <textarea id="new-comment-body" className="form-control w-100" ref={commentBodyRef} placeholder="Comment here..." maxLength="25000">
+
+        </textarea>
+        <button id="new-comment-btn" className="btn mt-1 btn-primary float-right" onClick={postComment}>
+            Comment
+        </button>
+    </React.Fragment>)
+}
 
 const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
     <a
@@ -21,7 +63,7 @@ const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
             e.preventDefault();
             onClick(e);
         }}>
-        <BsThreeDotsVertical/>
+        <BsThreeDotsVertical />
         {children}
     </a>
 ));
@@ -37,11 +79,13 @@ class PostComment extends Component {
         this.state = {
             commentInfo: {
                 reflink: this.props.reflink ? this.props.reflink : null
-            }
+            },
+            replying: false
         };
         this.handleAction = this.handleAction.bind(this)
     }
     async componentDidMount() {
+        console.log(this.props.reflink)
         let commentInfo;
         let profilePicture
         if (this.props.commentInfo) {
@@ -51,7 +95,7 @@ class PostComment extends Component {
         }
         if (commentInfo) {
             profilePicture = await Utils.accounts.getProfilePictureURL(commentInfo.reflink)
-
+            console.log(commentInfo)
             this.setState({
                 commentInfo,
                 profilePicture
@@ -60,7 +104,7 @@ class PostComment extends Component {
     }
     async handleAction(eventKey) {
         const reflink = this.state.commentInfo.reflink;
-        switch(eventKey) {
+        switch (eventKey) {
             case "block_post": {
                 await electronIpc.send("blocklist.add", reflink, {
                     reason: "manual block"
@@ -80,7 +124,7 @@ class PostComment extends Component {
             }
         }
     }
-    
+
     async postComment() {
         const profileID = localStorage.getItem('SNProfileID');
 
@@ -88,7 +132,7 @@ class PostComment extends Component {
             const profile = await utils.acctOps.getAccount(profileID);
             const accountType = 'hive';
             const theWifObj = Finder.one.in(user.keyring).with({
-                privateKeys: { }
+                privateKeys: {}
             })
             const theWif = theWifObj.privateKeys.posting_key
             const parentAuthor = ''; // ideally empty for blog posts
@@ -97,14 +141,20 @@ class PostComment extends Component {
             const permlink = ''; // post permalink
             const title = ''; // post title
             const body = ''; // post body or description 
-            const jsonMetadata = {tags: [''],  app: '' }
-    
-            const commentOp = {wif, parentAuthor, parentPermlink, author, permlink, title, body, jsonMetadata, accountType}
+            const jsonMetadata = { tags: [''], app: '' }
+
+            const commentOp = { wif, parentAuthor, parentPermlink, author, permlink, title, body, jsonMetadata, accountType }
             console.log(theWif);
             //await Utils.acctOps.postComment(commentOp)
         } else {
-            NotificationManager.success('You need to be logged in to perform this operation')
+            NotificationManager.error('You need to be logged in to perform this operation')
         }
+    }
+    get postTimeDistance() {
+        return NormaliseTime(new Date() - new Date(this.state.commentInfo.creation), "ms")
+    }
+    get postTime() {
+        return DateAndTime.format(new Date(this.state.commentInfo.creation), 'YYYY/MM/DD HH:mm:ss')
     }
     render() {
         return (<div>
@@ -119,8 +169,8 @@ class PostComment extends Component {
                         <Dropdown.Toggle as={CustomToggle}>
                         </Dropdown.Toggle>
                         <Dropdown.Menu>
-                            <Dropdown.Item style={{color:"red"}} eventKey="block_post">Block post</Dropdown.Item>
-                            <Dropdown.Item style={{color:"red"}} eventKey="block_user">Block user</Dropdown.Item>
+                            <Dropdown.Item style={{ color: "red" }} eventKey="block_post">Block post</Dropdown.Item>
+                            <Dropdown.Item style={{ color: "red" }} eventKey="block_user">Block user</Dropdown.Item>
                             <Dropdown.Item eventKey="copy_reflink">Copy to clipboard permlink</Dropdown.Item>
                         </Dropdown.Menu>
                     </Dropdown>
@@ -130,17 +180,43 @@ class PostComment extends Component {
                 <div className="panel ml-2 panel-default">
                     <div className="panel-heading ml-4">
 
-                        <strong><a href={`#/user?=${this.props.reflink}`}>{RefLink.parse(this.props.reflink).root}</a> </strong>• <span className="text-muted">6 days ago</span>
+                        <strong><a href={`#/user?=${this.props.reflink}`}>{RefLink.parse(this.props.reflink).root}</a> </strong>
+                        • <span className="text-muted">
+                            <OverlayTrigger
+                                overlay={
+                                    <Tooltip>
+                                        {this.postTime}
+                                    </Tooltip>
+                                }>
+                                <div>{this.postTimeDistance}</div>
+                            </OverlayTrigger></span>
                     </div>
                     <div className="panel-body mt-1">
                         <ReactMarkdown escapeHtml={false} source={DOMPurify.sanitize(this.state.commentInfo.description)}></ReactMarkdown>
                     </div>
                     <div className="panel-footer ml-0 ml-md-4">
                         {/*<FaDollarSign className="fa fa-dollar-sign" /> <span id="" className="post-payout">{this.state.commentInfo.payout}</span>
-                        <span id=""> • */}<Vote reflink={this.props.reflink} />{/* </span>*/}
+                        <span id=""> • */}
+                        <hr />
+                        <ul class="list-inline list-inline-separate">
+                            <li class="list-inline-item"><Vote reflink={this.props.reflink} /></li>
+                            <li class="list-inline-item" style={{ cursor: "pointer" }} onClick={(() => {
+                                this.setState({
+                                    replying: !this.state.replying
+                                })
+                            })}>
+                                {
+                                    this.state.replying ? "Cancel" : "Reply"
+                                }
+                            </li>
+                        </ul>
+                        {/* </span>*/}
                     </div>
                 </div>
             </div>
+            {
+                this.state.replying ? <div className="box mb-3 clearfix"><CommentForm parent_reflink={this.props.reflink} /></div> : null
+            }
         </div>);
     }
 }
