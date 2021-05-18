9d9182b00ca078fe5d672c2b7ae810244edf53a6
vaultec <47548474+vaultec81@users.noreply.github.com>
2021-05-17 18:40:20 -0700

    feat: implement custom url schema

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,5 @@
 import path from 'path';
-import {app, BrowserWindow} from 'electron';
+import { app, BrowserWindow } from 'electron';
 import debug from 'debug'
 import Core from './core'
 import ipcAdapter from './ipcAdapter'
@@ -11,7 +11,7 @@ const entryUrl = process.env.NODE_ENV === 'development'
   ? 'http://localhost:6789/index.html'
   : `file://${path.join(__dirname, 'index.html')}`;
 
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
   debug.enable("3speak:*");
 }
 
@@ -20,18 +20,47 @@ let coreInstance = new Core();
 
 const gotTheLock = app.requestSingleInstanceLock()
 
+app.removeAsDefaultProtocolClient("speak")
+app.setAsDefaultProtocolClient('speak', process.execPath);
+function devToolsLog(s) {
+  console.log(s)
+  if (window && window.webContents) {
+    window.webContents.executeJavaScript(`console.log("${s}")`)
+  }
+}
+app.on('open-url', (event, url) => {
+  // handle the data
+  console.log(event)
+  console.log(`url triggered on procotol: ${url}`)
+  devToolsLog('process args ' + url)
+  if (url.includes("speak://")) {
+    const UrlWoo = require("url").parse(url)
+    window.loadURL(entryUrl + UrlWoo.hash);
+  }
+});
+
+
 if (!gotTheLock) {
   app.quit()
 } else {
-  app.on('second-instance', (event, commandLine, workingDirectory) => {
+  app.on('second-instance', (event, argv, workingDirectory) => {
     if (window) {
       if (window.isMinimized()) window.restore()
       window.focus()
+      const theUrl = argv[argv.length - 1];
+      if (theUrl.includes("speak://")) {
+        const UrlWoo = require("url").parse(theUrl)
+        window.loadURL(entryUrl + UrlWoo.hash);
+      } else {
+        window.loadURL(entryUrl);
+      }
     }
   })
 
+  console.log(process.argv)
   app.on('ready', () => {
-    window = new BrowserWindow({width: 800, height: 600,
+    window = new BrowserWindow({
+      width: 800, height: 600,
       icon: path.resolve(__dirname, "../renderer/assets/img/app.png"),
       webPreferences: {
         nodeIntegration: true,
@@ -39,14 +68,20 @@ if (!gotTheLock) {
         webviewTag: true
       }
     });
-    window.loadURL(entryUrl);
+    const theUrl = process.argv[process.argv.length - 1];
+    if (theUrl.includes("speak://")) {
+      const UrlWoo = require("url").parse(theUrl)
+      window.loadURL(entryUrl + UrlWoo.hash);
+    } else {
+      window.loadURL(entryUrl);
+    }
     window.on('closed', () => window = null);
   });
 }
 
 app.commandLine.appendSwitch('disable-features', 'OutOfBlinkCors');
 app.on('window-all-closed', async () => {
-  if(process.platform !== 'darwin') {
+  if (process.platform !== 'darwin') {
     await coreInstance.stop();
     app.quit();
   }
