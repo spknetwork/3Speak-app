4465c91a092d96137e3e2ecd0dd0b28f515d3d76
nathansenn <nathan.senn90@gmail.com>
2023-04-28 22:50:01 +0800

    Fix for pin page issue

--- src/renderer/views/PinsView/PinRows.tsx
+++ src/renderer/views/PinsView/PinRows.tsx
@@ -9,35 +9,41 @@ export const pinRows = (pinList: any[], removePin: any) => {
   return (
     <>
       {pinList.map((pin) => {
-        const sizeBest = bytesAsString(pin.size);
-        const expireText = pin.expire
-          ? `In ${millisecondsAsString((pin.expire = new Date().getTime()))}`
-          : 'Permanent';
-        const pinDateText = pin.meta.pin_date ? new Date(pin.meta.pin_date).toLocaleString() : null;
+          if(pin.meta) {
+          console.log('pinRows.tsx pin: ', pin);
+            const sizeBest = bytesAsString(pin.size);
+            const expireText = pin.expire
+              ? `In ${millisecondsAsString((pin.expire = new Date().getTime()))}`
+              : 'Permanent';
+            const pinDateText = pin.meta.pin_date ? new Date(pin.meta.pin_date).toLocaleString() : null;
+            console.log('pinRows.tsx pinDateText: ', pinDateText);
+            return (
+              <tr key={pin._id}>
+                <td>
+                  <a href={`#/watch/${pin._id}`}>{pin._id}</a>
+                  <br />(<strong>{RefLink.parse(pin._id).root}</strong>)
+                </td>
+                <td>
+                  <a href={`#/watch/${pin._id}`}>{pin.meta ? pin.meta.title : null} </a>
+                </td>
+                <td>
+                  <PinCids pin={pin} />
+                </td>
+                <td>{pin.source}</td>
+                <td>{expireText}</td>
+                <td>{pinDateText}</td>
+                <td>{pin.size === 0 ? <strong>Pinning In Progress</strong> : sizeBest}</td>
+                <td>
+                  <Button variant="danger" onClick={() => removePin(pin._id)}>
+                    X
+                  </Button>
+                </td>
+              </tr>
+            );
 
-        return (
-          <tr key={pin._id}>
-            <td>
-              <a href={`#/watch/${pin._id}`}>{pin._id}</a>
-              <br />(<strong>{RefLink.parse(pin._id).root}</strong>)
-            </td>
-            <td>
-              <a href={`#/watch/${pin._id}`}>{pin.meta ? pin.meta.title : null} </a>
-            </td>
-            <td>
-              <PinCids pin={pin} />
-            </td>
-            <td>{pin.source}</td>
-            <td>{expireText}</td>
-            <td>{pinDateText}</td>
-            <td>{pin.size === 0 ? <strong>Pinning In Progress</strong> : sizeBest}</td>
-            <td>
-              <Button variant="danger" onClick={() => removePin(pin._id)}>
-                X
-              </Button>
-            </td>
-          </tr>
-        );
+          }else {
+            console.log('pinRows.tsx pin is null');
+          }
       })}
     </>
   );
