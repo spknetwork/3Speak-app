a5caadc4f58d099cac5856548e8a2429d950576b
vaultec <47548474+vaultec81@users.noreply.github.com>
2023-05-07 22:08:00 -0700

    partial: dag import ipfs

--- src/main/core/components/Pins.ts
+++ src/main/core/components/Pins.ts
@@ -1,8 +1,14 @@
-import { CID, IPFSHTTPClient } from 'ipfs-http-client'
+import { CID, globSource, IPFSHTTPClient } from 'ipfs-http-client'
 import PouchDB from 'pouchdb'
+import Axios from 'axios'
+import fs from 'fs'
+import fsPromises from 'fs/promises'
 import { CoreService } from '..'
 import RefLink from '../../RefLink'
 import { IpfsHandler } from './ipfsHandler'
+import tmp from 'tmp'
+import execa from 'execa'
+import waIpfs from 'wa-go-ipfs'
 const Path = require('path')
 const debug = require('debug')('3speak:pins')
 const Schedule = require('node-schedule')
@@ -10,18 +16,73 @@ PouchDB.plugin(require('pouchdb-find'))
 PouchDB.plugin(require('pouchdb-upsert'))
 
 async function progressPin(ipfs: IPFSHTTPClient, pin: string, callback: Function) {
-  const refs = ipfs.refs(pin, {
-    recursive: true
-    // maxDepth: 1
+
+  // ipfs.dag.exp
+  
+  const tmpPath = tmp.dirSync();
+  console.log(tmpPath)
+  const writer = fs.createWriteStream(Path.join(tmpPath.name, 'download.car'));
+  
+  await Axios.get(`https://ipfs-3speak.b-cdn.net/api/v0/dag/export?arg=${pin}`, {
+    responseType: 'stream'
+  }).then(response => {
+
+    return new Promise((resolve, reject) => {
+      response.data.pipe(writer);
+      let error = null;
+      writer.on('error', err => {
+        error = err;
+        writer.close();
+        reject(err);
+      });
+      writer.on('close', () => {
+        if (!error) {
+          resolve(true);
+        }
+        
+      });
+    });
   })
-  const TotalSize = (await ipfs.object.stat(CID.parse(pin))).CumulativeSize
-  let counter = 0
-  for await(let result of refs) {
-    // const CumulativeSize = (await ipfs.object.stat(CID.parse(result.ref))).CumulativeSize
-    const dag = await ipfs.dag.get(CID.parse(result.ref))
-    counter = counter + dag.value.Data.length;
-    callback(Number((counter / TotalSize).toFixed(3)))
-  }
+  
+  // console.log('got here')
+  // for await(let importBlock of ipfs.dag.import(fs.createReadStream(Path.join(tmpPath.name, 'download.car')))) {
+  //   console.log(importBlock)
+  // }
+
+  const ipfsInfo = await IpfsHandler.getIpfs()
+  const ipfsPath = await waIpfs.getPath(
+    waIpfs.getDefaultPath({ dev: process.env.NODE_ENV === 'development' }),
+  )
+
+
+  const output = await execa(ipfsPath, [
+    'dag',
+    'import',
+    Path.join(tmpPath.name, 'download.car')
+  ], {
+    env: {
+      IPFS_PATH: ipfsInfo.ipfsPath
+    }
+  })
+  console.log(output)
+  
+  await fsPromises.rmdir(tmpPath.name, {
+    recursive: true,
+    force: true
+  } as any)
+
+  // const refs = ipfs.refs(pin, {
+  //   recursive: true
+  //   // maxDepth: 1
+  // })
+  // const TotalSize = (await ipfs.object.stat(CID.parse(pin))).CumulativeSize
+  // let counter = 0
+  // for await(let result of refs) {
+  //   // const CumulativeSize = (await ipfs.object.stat(CID.parse(result.ref))).CumulativeSize
+  //   const dag = await ipfs.dag.get(CID.parse(result.ref))
+  //   counter = counter + dag.value.Data.length;
+  //   callback(Number((counter / TotalSize).toFixed(3)))
+  // }
 }
 class Pins {
   self: CoreService
@@ -84,6 +145,7 @@ class Pins {
       await progressPin(this.self.ipfs, cid, (percent) => {
         totalPercent = percent
       })
+      console.log('Done pinning, attempting full pin')
       await this.self.ipfs.pin.add(cid)
       console.log('Getting Cumulative size of', cid, new Date(), doc._id)
       const objectInfo = await this.self.ipfs.object.stat(cid)
